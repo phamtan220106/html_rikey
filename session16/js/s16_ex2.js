8c7177ffef79bb58f05dc390a74e839f0bf2f198
phamtan220106 <tanpham220106z@gmail.com>
2025-02-26 16:14:57 +0700

    sesion16

--- session16/js/s16_ex2.js
+++ session16/js/s16_ex2.js
@@ -2,4 +2,7 @@ let number = 10;
 let price = 20.5;
 let statust = true;
 let city = "hà Nội";
-document.write(number + "<br>", price + "<br>", statust + "<br>", city + "<br>");
+document.write("Biến <b>number</b> kiểu số nguyên, có giá trị là <b>" + number + "</b><br>");
+document.write("Biến <b>price</b> kiểu số thực, có giá trị là <b>" + price + "</b><br>");
+document.write("Biến <b>statust</b> kiểu logic, có giá trị là <b>" + statust + "</b><br>");
+document.write("Biến <b>city</b>  kiểu chuỗi, có giá trị là <b>" + city + "</b><br>");
